3af0f3be69110a7c90a7a008463baf762fe6aae5
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-06-10 16:53:55 +0200

    debugging

--- js/interface.js
+++ js/interface.js
@@ -1,58 +1,59 @@
-let dynamicContainer = null;
-let htmlToShow = '';
-let dataSourceObj = null;
+// let dynamicContainer = null;
+// let htmlToShow = '';
+// let dataSourceObj = null;
 
-Fliplet.Widget.findParents({ filter: { package: 'com.fliplet.dynamic-container' } }).then(async widgets => {
-  if (widgets.length === 0) {
-    htmlToShow = '<p style="font-size: 10px; font-weight: 400; color: #E7961E;">To change Data source go to Dynamic Container Settings on the LFD page</p>';
-  } else {
-    dynamicContainer = widgets[0];
-    dataSourceObj = await Fliplet.DataSources.getById(dynamicContainer && dynamicContainer.dataSourceId, {
-      attributes: ['name']
-    }).then(dataSource => {
-      return dataSource;
-    });
+// Fliplet.Widget.findParents({ filter: { package: 'com.fliplet.dynamic-container' } }).then(async widgets => {
+//   if (widgets.length === 0) {
+//     htmlToShow = '<p style="font-size: 10px; font-weight: 400; color: #E7961E;">To change Data source go to Dynamic Container Settings on the LFD page</p>';
+//   } else {
+//     dynamicContainer = widgets[0];
+//     dataSourceObj = await Fliplet.DataSources.getById(dynamicContainer && dynamicContainer.dataSourceId, {
+//       attributes: ['name']
+//     }).then(dataSource => {
+//       return dataSource;
+//     });
 
-    htmlToShow = `<p style="color: #A5A5A5; font-size: 12px; font-weight: 400;">List from ${dataSourceObj.name}(ID: <span class="data-source-id">${dynamicContainer.dataSourceId}</span>)</p>
-    <p style="font-size: 10px; font-weight: 400; color: #E7961E;">To change Data source go to Data Container Settings</p>
-    <hr/>`;
-  }
+//     htmlToShow = `<p style="color: #A5A5A5; font-size: 12px; font-weight: 400;">List from ${dataSourceObj.name}(ID: <span class="data-source-id">${dynamicContainer.dataSourceId}</span>)</p>
+//     <p style="font-size: 10px; font-weight: 400; color: #E7961E;">To change Data source go to Data Container Settings</p>
+//     <hr/>`;
+//   }
 
-  return Fliplet.Widget.generateInterface({
-    title: 'Filter container',
-    fields: [
-      {
-        type: 'html',
-        html: htmlToShow
+//   return
+Fliplet.Widget.generateInterface({
+  title: 'Filter container',
+  fields: [
+    // {
+    //   type: 'html',
+    //   html: htmlToShow
+    // },
+    // {
+    //   name: 'isListOnDifferentScreen',
+    //   type: 'checkbox',
+    //   label: 'List is on the different screen than filter',
+    //   options: [{ value: true, label: 'yes' }],
+    //   default: [],
+    //   change: function(value) {
+    //     // Fliplet.Helper.field('action').toggle(value.includes(true));
+    //   },
+    //   ready: function() {
+    //     // Fliplet.Helper.field('action').toggle(
+    //     //   Fliplet.Helper.field('isListOnDifferentScreen').get().includes(true)
+    //     // );
+    //   }
+    // },
+    {
+      name: 'action',
+      type: 'provider',
+      label: 'Choose an action to do when the button is pressed',
+      package: 'com.fliplet.link',
+      onEvent: function(event, payload) {
+        debugger;
       },
-      {
-        name: 'isListOnDifferentScreen',
-        type: 'checkbox',
-        label: 'List is on the different screen than filter',
-        options: [{ value: true, label: 'yes' }],
-        default: [],
-        change: function(value) {
-          // Fliplet.Helper.field('action').toggle(value.includes(true));
-        },
-        ready: function() {
-          // Fliplet.Helper.field('action').toggle(
-          //   Fliplet.Helper.field('isListOnDifferentScreen').get().includes(true)
-          // );
-        }
-      },
-      {
-        name: 'action',
-        type: 'provider',
-        label: 'Choose an action to do when the button is pressed',
-        package: 'com.fliplet.link',
-        onEvent: function(event, payload) {
-          debugger;
-        },
-        ready: function(x, y) {
-          debugger
-          // Fliplet.Helper.field('action').set(y);
-        }
+      ready: function(x, y) {
+        debugger;
+        // Fliplet.Helper.field('action').set(y);
       }
-    ]
-  });
+    }
+  ]
 });
+// });
